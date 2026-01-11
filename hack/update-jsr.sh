e02e2a9e09778b9d02a49c44189e53a3a06fe231
Daniel Lamando <dan@danopia.net>
2026-01-11 21:52:51 +0100

    Use more concise jsr update script

--- hack/update-jsr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -eux
-cd src
-
-deno run \
-  --allow-read=. \
-  --allow-write=deno.json \
-  https://raw.githubusercontent.com/oscarotero/jsr-pub/4d1ef6e25b02fc26551fa7e5d29bbdc5e9884050/mod.ts \
-  --name "$(jq -r .name < deno.json)" \
-  --version "$(jq -r .version < deno.json)"
