f39db01da022c287caa7ab83408955cf5d5bdd6a
Daniel Lamando <dan@danopia.net>
2026-07-31 13:49:48 +0200

    Wrap setInterval callback for canary

--- src/sources/from-timer.ts
+++ src/sources/from-timer.ts
@@ -29,6 +29,6 @@ export function fromTimer(ms: number): Observable<null> {
     }
     clearInterval(timer);
   });
-  timer = setInterval(next, ms);
+  timer = setInterval(() => next(), ms);
   return observable;
 }
